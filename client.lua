f1220da4a2d52de3dcb41cb6d69b467f6d39c1cc
Iman <iman.naeimian@gmail.com>
2025-06-03 21:48:53 +0330

    Initial commit - loading screen

---
+++ client.lua
@@ -0,0 +1,9 @@
+AddEventHandler("playerSpawned", function()
+    print("[NV] - Loading Screen Off ")
+    ShutdownLoadingScreen()
+    ShutdownLoadingScreenNui()
+end)
+-- Deaveloper_______________________
+-- https://imannaeimian.site/
+-- github.com/ImanNaeimian
+-- _________________________________  
---
+++ html/config.js
@@ -0,0 +1,94 @@
+var staff_team = [
+
+	{
+		"name":"RealVictor",
+		"image":"https://forum.cfx.re/user_avatar/forum.cfx.re/neveradevelopment/144/4447132_2.png",
+		"rank":"Owner"
+	},
+
+]
+var Developer_team = [
+
+	{
+		"name":"RealVictor",
+		"image":"https://forum.cfx.re/user_avatar/forum.cfx.re/neveradevelopment/144/4447132_2.png",
+		"rank":"Developer"
+	},
+
+
+]
+
+
+// Staff Settings
+const showStaffTeam = true
+const showDeveloperTeam = true
+const showPlayersList = true
+
+
+// orange
+// red
+// blue
+// green
+// pink
+// purple
+const theme = "red"
+
+// ==== WINTER MODE ==== \\
+const enableWinterUpdate = false
+// ==== WINTER MODE : you can chose false or true ==== \\
+
+
+// Text settings
+const name = "<strong>GH</strong>OST"
+const underName = "ROLE<b>PLAY</b>"
+const desc = "FiveM Roleplay server featuring an extensive collection of custom scripts, maps, vehicles, unique weapons, and much more."
+
+
+// Social media
+const discord = "https://discord.gg/BP2SZhdz"
+const instagram = ""
+const youtube = "" 
+const twitter = ""
+const tiktok = ""
+const facebook = ""
+const twitch = ""
+const github = "https://github.com/ImanNaeimian"
+
+
+
+// Player List
+const serverCode = "" //Your server CODE from fivem. (cfx.re/join/abcdef) this ABCDEF is your code, paste it in serverCode. 
+const playerProfileImage = "https://forum.cfx.re/user_avatar/forum.cfx.re/neveradevelopment/144/4447132_2.png"
+
+
+// Video Settings
+const videoBlur = 0
+var videoOpacity = 0.3
+
+
+// Example link: https://www.youtube.com/watch?v
+const youtubeVideo = ""
+const showYoutubeVideo = false
+
+// Local Video
+const enableLocalVideo = false
+
+// Local audio
+const localAudio = true
+// Deaveloper_______________________
+// https://imannaeimian.site/
+// github.com/ImanNaeimian
+//_________________________________  
+
+
+// HELP //
+
+//-- YOUTBE
+//-- LOCAL AUDIO
+// if localAudio is true, then loading will load "audio.mp3" file and play it except youtube audio.
+// if localAudio is false, then loading will load youtube audio.
+
+//-- LOCAL VIDEO
+// if enableLocalVideo is true, then loading will load "video.webm" file and play it except youtube video.
+// If localVideo is enabled, showYoutubeVideo is automatically disabled.
+// You can only import a video from either YouTube or local. Local video taking priority.
